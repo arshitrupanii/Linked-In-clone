e627fad2b4fa19d0b16cc9b837d5b6ac762f7e9b
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2025-02-12 08:52:24 +0530

    u

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 
 createRoot(document.getElementById('root')).render(
@@ -18,3 +15,4 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
