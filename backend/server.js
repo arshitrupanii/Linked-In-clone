8e361556e86996bde44802897251f20d6dd2071b
arshitrupanii <arshitrupani6@gmail.com>
2025-02-12 08:49:05 +0530

    u

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import notificationRoutes from "./routes/notification.routes.js"
 import connectionsRoutes from "./routes/connections.routes.js"
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -14,7 +15,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.json()) //
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+));
+app.use(express.json({limit: '50mb'})); 
 app.use(cookieParser());
 
 app.use("/api/v1/auth" , authRoutes)
